f4a91e1211d9a8743c99e6480fae8a4d7d7cc4e8
I-Asous <islamasous@gmail.com>
2025-08-30 21:14:35 -0400

    Initial Commit

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Islam Asous on 8/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    //connect button/label to view controller to create action
+    //Solid circle establishes relation
+    @IBAction func showAlert(){
+        //Implementing the actual alert display functionality
+        let alert = UIAlertController(
+            title: "Hello World",
+            message: "This is my first app!",
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "Awesome",
+            style: .default,
+            handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
