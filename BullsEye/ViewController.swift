8b6be187e1c4f846dedecb4a13e6967c2a00bb51
I-Asous <islamasous@gmail.com>
2025-09-02 11:40:06 -0400

    random number is now displayed. implemented using int.random()

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -8,27 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController{
+    //outlet<-tells Interface Builder that you now have a variable named slider that can be connected to a UISlider object
+    @IBOutlet var slider: UISlider!
+    
     //customary to add the variables above the methods
-    var currentValue: Int = 0
+    var targetValue = 0
+    var currentValue: Int = 50
     override func viewDidLoad() {
         super.viewDidLoad()
+        //currentValue change from 0 to 50 to portray resting position
+        currentValue = lroundf(slider.value)
+        //call the random() function built into Int to get an arbitrary integer (whole number) between 1 and 100
+        targetValue = Int.random(in: 1...100)
     }
     //connect button/label to view controller to create action
     //Solid circle establishes relation
     
     //Methods start with the word func
     @IBAction func showAlert(){
+        let message = "The value of the slider is:  \(currentValue)" + "\nThe target value is: \(targetValue)"
+        
         //Implementing the actual alert display functionality
         //Post hitting the "Hit me!" button
         let alert = UIAlertController(
             title: "Hello World",
-            message: "This is my first app!",
+            message: message,
             preferredStyle: .alert)
         
         let action = UIAlertAction(
-            title: "Awesome",
+            title: "Ok",
             style: .default,
             handler: nil)
+        
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
