9fffbac7d1386ef381b61f9d743c645857d0434b
I-Asous <islamasous@gmail.com>
2025-09-04 15:12:50 -0400

    upoated to have random number target appear

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -16,16 +16,20 @@ class ViewController: UIViewController{
     var currentValue: Int = 50
     override func viewDidLoad() {
         super.viewDidLoad()
+        startNewRound()
+        /*
         //currentValue change from 0 to 50 to portray resting position
         currentValue = lroundf(slider.value)
         //call the random() function built into Int to get an arbitrary integer (whole number) between 1 and 100
         targetValue = Int.random(in: 1...100)
+         */
     }
     //connect button/label to view controller to create action
     //Solid circle establishes relation
     
     //Methods start with the word func
     @IBAction func showAlert(){
+        //+ to glue different bits of text into one big string
         let message = "The value of the slider is:  \(currentValue)" + "\nThe target value is: \(targetValue)"
         
         //Implementing the actual alert display functionality
@@ -42,6 +46,12 @@ class ViewController: UIViewController{
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
+        //Display
+    }
+    func updateLabels() {
+    targetLabel.text = String(targetValue)
+    //can also be written using string interpolation: targetLabel.text = "\(targetValue)"
     }
     
     //Functionality for the slider <- indicates current value of slider
@@ -49,5 +59,17 @@ class ViewController: UIViewController{
         //print("The value of the slider is now: \(slider.value)")
         //lroundf() <-round decimal number to the nearest whole number then store that rounded-off number in currentValue
         currentValue = lroundf(slider.value)
+    }
+    
+    @IBOutlet var targetLabel: UILabel!
+    
+    //Functionality to start a new round
+    func startNewRound(){
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        //Swift is a strongly typed language, meaning that it is really picky about the shapes that you can put into the boxes
+        //Swift wants you to be explicit about such conversions
+        slider.value = Float(currentValue)
+        updateLabels()
     }
 }
